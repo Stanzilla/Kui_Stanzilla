ae15e97f570702a2b75b6c64699578d320e540c8
Benjamin Staneck <staneck@gmail.com>
2018-10-25 17:09:04 +0200

    oops

--- custom.agony.lua
+++ custom.agony.lua
@@ -1,1 +1,29 @@
-../Fleischpflanzerl/Modules/custom.agony.lua
+-- adds an agony tracker to the left of the nameplate
+local addon = KuiNameplates
+local core = KuiNameplatesCore
+
+local mod = addon:NewPlugin('AgonyAura',101,3)
+if not mod then return end
+
+function mod:Create(f)
+    local agony = f.handler:CreateAuraFrame({
+        max = 1,
+        size = 36,
+        squareness = 1,
+        point = {'BOTTOMRIGHT','RIGHT','LEFT'},
+        rows = 1,
+        filter = 'HARMFUL|PLAYER',
+        whitelist = {
+            [980] = true, -- agony
+        },
+    })
+    agony:SetFrameLevel(0)
+    agony:SetWidth(32)
+    agony:SetHeight(32)
+    agony:SetPoint('LEFT',f.bg,'LEFT',-40,0)
+    f.AgonyAura = agony
+end
+
+function mod:Initialise()
+    self:RegisterMessage('Create')
+end
